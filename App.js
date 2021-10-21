6c77c5c56a2a81fde9d051fba52624475cf52898
Steven <bongamke@sheridancollege.ca>
2021-10-20 23:23:50 -0400

    Button Component created

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <CarItem />
-
+      
 
       <StatusBar style="auto" />
     </View>
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import { View, Text, ImageBackground } from 'react-native'
 import styles from './styles'
+import StyledButton from '../StyledButton'
 
 const CarItem = () => {
     return (
         <View style={styles.carContainer}>
 
-        <ImageBackground
-          source={require('../../assets/images/ModelX.jpeg')}
-          style={styles.image}
-          />
+            <ImageBackground
+                source={require('../../assets/images/ModelX.jpeg')}
+                style={styles.image}
+            />
           
-          <View style={styles.titles}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subTitle}>Starting at $69,420</Text>
-          </View>
+            <View style={styles.titles}>
+                <Text style={styles.title}>Model S</Text>
+                <Text style={styles.subTitle}>Starting at $69,420</Text>
+            </View>
+
+            <StyledButton type="primary"
+                content={"Custom Order"}
+                onPress={() => {
+                    console.warn('Custom Order was pressed')
+                }} />
+            <StyledButton type="secondary"
+                content={"Existing Inventory"}
+                onPress={() => {
+                    console.warn('Existing Inventory was pressed')
+                }} />
+            
+            
+
        
       </View>
     )
